129c2e60b3166effe040423ffc19c6e4af8fd7bd
yahyacodes <yahya.codin@gmail.com>
2024-01-08 17:42:01 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    return f'<h1>The host for page is {host}</h1>'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
